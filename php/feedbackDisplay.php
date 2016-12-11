7a5038e04ad6b0619b02e42376acab16be651b13
Adam Rowe <adam1rowe1@gmail.com>
2016-12-11 14:54:30 +0000

    Final Commit
    This is the final project includes updates

--- php/feedbackDisplay.php
+++ php/feedbackDisplay.php
@@ -9,27 +9,7 @@
   <script src="../js/bootstrap.min.js"></script>
   </head>
     <body> 
-      <nav class="navbar navbar-inverse">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>                        
-      </button>
-      <a class="navbar-brand" href="#"><span class="glyphicon glyphicon-shopping-cart"></span></a>
-    </div>
-    <div class="collapse navbar-collapse" id="myNavbar">
-      <ul class="nav navbar-nav">
-        <li  class="active"><a href="#">Home</a></li>
-        <li><a href="product.html">Products</a></li>
-        <li><a href="Deals.html">Deals</a></li>
-        <li><a href="#">Staff</a></li>
-        <li><a href="Contact.html">Contact</a></li>
-      </ul>
-    </div>
-  </div>
-</nav>
+
     <div class="container">
     <h2>Feedback</h2> 
     <div class="panel panel-default"> 
@@ -46,7 +26,7 @@
       
       <style>
          body{
-      background-image: url("/WebAppCA3/back.png");  
+      background-image: url("back.png");  
     }
       
         h2{
